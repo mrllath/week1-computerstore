17ee4634700fc8ae3865b98c20559fc814c4327d
Hans <Hans@192.168.0.247>
2017-09-28 18:11:14 +0200

    Taak 3 afgewerkt

--- src/be/pxl/computerstore/hardware/Hardware.java
+++ src/be/pxl/computerstore/hardware/Hardware.java
@@ -1,33 +1,68 @@
 package be.pxl.computerstore.hardware;
 
+import java.util.Random;
+
 public abstract class Hardware {
-	
+
 	private String vendor;
 	private String name;
 	private double price;
+	private String articleNumber;
+	private static Random randomNumberGenerator = new Random();
+	private static int randomNumber = 10000 + randomNumberGenerator.nextInt(99999);
+	private static int [] nums =  {1,2,3,4,5,6,7,8,9};
+	private static StringBuilder sb = new StringBuilder();
 	
 	
 	public Hardware(String vendor, String name, double price) {
-		super();
 		this.vendor = vendor;
 		this.name = name;
 		this.price = price;
+		if (this.vendor.length() > 3) {
+			this.articleNumber = this.vendor.substring(0, 3).toUpperCase();
+		} else {
+			this.articleNumber = this.vendor.toUpperCase() + "X";
+		}
+		this.articleNumber += "-";
+		this.articleNumber += randomNumber;
+		this.articleNumber += "-";
+		for (int i = 0; i < 3; i++) {
+			sb = sb.append(nums[randomNumberGenerator.nextInt(9)]);
+		}
+		this.articleNumber += sb;
 	}
+
+
+	public String getArticleNumber() {
+		return articleNumber;
+	}
+
+
+	public void setArticleNumber(String articleNumber) {
+		this.articleNumber = articleNumber;
+	}
+
+
 	public String getVendor() {
 		return vendor;
 	}
+
 	public void setVendor(String vendor) {
 		this.vendor = vendor;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public double getPrice() {
 		return price;
 	}
+
 	public void setPrice(double price) {
 		this.price = price;
 	}
